94144c5d3c4754485bfd5f220f3a0f80eb5c9c0c
ZeddZoo <ziyizuo@andrew.cmu.edu>
2020-01-15 17:47:11 -0500

    Incorportate OPENSV stuffs

---
+++ Heatmap Generator/CMakeFiles/cvInterface.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cvInterface.dir/cvInterface.cpp.o"
+  "cvInterface.pdb"
+  "cvInterface"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cvInterface.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
--- Heatmap Generator/cvInterface.cpp
+++ Heatmap Generator/cvInterface.cpp
@@ -14,6 +14,7 @@ class cvInterface {
   public:
     // Returns a vector of all the people in the image as Point2f
     static std::vector<cv::Point2f> getPersonPoints(std::string source) {
+      std::vector<cv::Point2f> persons;
       // Load in the image from the source
       // TODO@{BEN}: Make the loading in of the image make sense
       cv::Mat image = cv::imread(
@@ -22,10 +23,13 @@ class cvInterface {
       std::vector<cv::Vec3f> circles;
       cv::HoughCircles(image, circles, cv::HOUGH_PROBABILISTIC, 1, image.rows/8, 200, 100, 0, 0);
 
-      /// Draw the circles detected
+      cv::GaussianBlur(image, image, cv::Size(9, 9), 2, 2 );
+
+      /// Draw the circles detected and add their points to the person vector
       for( size_t i = 0; i < circles.size(); i++ )
       {
           cv::Point center(cvRound(circles[i][0]), cvRound(circles[i][1]));
+          persons.push_back(cv::Point2f(cvRound(circles[i][0]), cvRound(circles[i][1])));
           int radius = cvRound(circles[i][2]);
           // circle center
           cv::circle( image, center, 3, cv::Scalar(0,255,0), -1, 8, 0 );
