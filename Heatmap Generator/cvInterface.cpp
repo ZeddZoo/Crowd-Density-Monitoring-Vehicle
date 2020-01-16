734807ec9271fd5f5b18fe5cb46dfedd8ba2df92
ZeddZoo <ziyizuo@andrew.cmu.edu>
2020-01-16 16:39:28 -0500

    Finish up feature detection

--- Heatmap Generator/cvInterface.cpp
+++ Heatmap Generator/cvInterface.cpp
@@ -9,6 +9,8 @@
 #include <opencv2/video/background_segm.hpp>
 #include <vector>
 
+#define TODISP image
+
 // #include "opencv/modules/features2d/include/opencv2/features2d.hpp"
 // #include "opencv/modules/highgui/include/opencv2/highgui.hpp"
 // #include "opencv/modules/imgproc/include/opencv2/imgproc.hpp"
@@ -21,9 +23,9 @@ class cvInterface {
       // Load in the image from the source
       // TODO@{BEN}: Make the loading in of the image make sense
       cv::Mat image = cv::imread(
-                cv::samples::findFile("test4.jpg"), cv::IMREAD_COLOR);
-      cv::subtract(image, tenSecDiff, image);
+                cv::samples::findFile("test7HD.jpg"), cv::IMREAD_COLOR);
       cv::Mat original = image.clone();
+      cv::subtract(tenSecDiff, image, image);
       image.convertTo(image, -1, 1.25, -10);
       double alpha = 1.1;
       cv::Mat black = cv::Mat(image.rows, image.cols, image.type(), 0.0);
@@ -34,12 +36,12 @@ class cvInterface {
 
       std::vector<cv::Vec3f> circles;
 
-      cv::GaussianBlur(image, image, cv::Size(3, 3), 2, 2 );
+      cv::GaussianBlur(image, image, cv::Size(5, 5), 1, 1);
 
-      // // Apply edge detection to eliminate uneeded details
-      // cv::Canny(image, image, 100, 200);
-      // // Filter out uneeded horizontal and vertical edges
-      // // cv::Mat horizontal = image.clone();
+      // Apply edge detection to eliminate uneeded details
+      cv::Canny(image, image, 100, 200);
+      // Filter out uneeded horizontal and vertical edges
+      // cv::Mat horizontal = image.clone();
       // cv::Mat vertical = image.clone();
 
       // // Specify size on horizontal axis
@@ -57,7 +59,6 @@ class cvInterface {
       // erode(vertical, vertical, verticalStructure, cv::Point(-1, -1));
       // dilate(vertical, vertical, verticalStructure, cv::Point(-1, -1));
       // image = image - vertical - horizontal;
-      // image = vertical;
 
       // cv::Mat element = cv::getStructuringElement(0, cv::Size(1, 1), cv::Point(0, 0));
       // cv::Mat lines;
@@ -65,15 +66,15 @@ class cvInterface {
       // cv::morphologyEx(image, image, cv::MORPH_OPEN, element);
       // image -= lines;
       // // cv::subtract(element, lines, element);
-      cv::Mat imageStructure = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
+      // cv::Mat imageStructure = cv::getStructuringElement(cv::MORPH_ELLIPSE, cv::Size(3, 3));
 
-      cv::morphologyEx(image, image, cv::MORPH_CLOSE, imageStructure);
+      // cv::morphologyEx(image, image, cv::MORPH_CLOSE, imageStructure);
       // cv::erode(image, image, imageStructure, cv::Point(-1, -1));
       // cv::dilate(image, image, imageStructure, cv::Point(-1, -1));
       // cv::erode(image, image, imageStructure, cv::Point(-1, -1));
 
 
-      cv::HoughCircles(image, circles, cv::HOUGH_GRADIENT, 1, image.rows/10, 150, 20, 20, 100);
+      cv::HoughCircles(image, circles, cv::HOUGH_GRADIENT, 1, image.rows/10, 250, 20, 25, 60);
 
 
 
@@ -84,14 +85,17 @@ class cvInterface {
           persons.push_back(cv::Point2f(cvRound(circles[i][0]), cvRound(circles[i][1])));
           int radius = cvRound(circles[i][2]);
           // circle center
-          cv::circle(image, center, 3, cv::Scalar(0,0,255), -1, 8, 0 );
+          cv::circle(TODISP, center, 3, cv::Scalar(0,0,255), -1, 8, 0 );
           // circle outline
-          cv::circle(image, center, radius, cv::Scalar(0,0,0), 2, 8, 0 );
+          cv::circle(TODISP, center, radius, cv::Scalar(255,255,0), 2, 8, 0 );
       }
 
       // / Show your results
+      // Scale the result
+      double sF = .5;
+      cv::resize(TODISP, TODISP, cv::Size(TODISP.cols * sF, TODISP.rows * sF), 0, 0, cv::INTER_LINEAR);
       cv::namedWindow( "Hough Circle Transform Demo", cv::WINDOW_AUTOSIZE );
-      cv::imshow( "Hough Circle Transform Demo", image );
+      cv::imshow( "Hough Circle Transform Demo", TODISP);
 
       cv::waitKey(0);
     }
@@ -99,6 +103,6 @@ class cvInterface {
 
 int main() {
   cv::Mat tenSecDiff = cv::imread(
-                cv::samples::findFile("test3.jpg"), cv::IMREAD_COLOR);
+                cv::samples::findFile("test6HD.jpg"), cv::IMREAD_COLOR);
   cvInterface::getPersonPoints("hello", tenSecDiff);
 }
