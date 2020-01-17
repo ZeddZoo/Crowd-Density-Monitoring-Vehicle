93fb1355ed3888ce40862c29270b8b4dc0d1d4ee
Ben's Pi <benwilliamgraham@gmail.com>
2020-01-16 20:37:09 -0500

    a

---
+++ pihost.py
@@ -0,0 +1,24 @@
+import picamera
+import io
+import time
+import socket
+
+camera = picamera.PiCamera(resolution="1280x720", framerate=5)
+stream = io.BytesIO()
+camera.start_preview()
+time.sleep(1.5)
+
+HOST = ""
+PORT = 8000
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
+    sock.bind((HOST, PORT))
+    sock.listen()
+    connection, address = sock.accept()
+    with connection:
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            camera.capture(stream, "rgba")
+            connection.sendall(stream.getvalue())
