952c3f1eb949ff9513853f5ab0f37d37fc8505d8
Ben's Pi <benwilliamgraham@gmail.com>
2020-01-16 22:38:53 -0500

    a

--- pihost.py
+++ pihost.py
@@ -3,7 +3,7 @@ import io
 import time
 import socket
 
-camera = picamera.PiCamera(resolution="1280x720", framerate=5)
+camera = picamera.PiCamera(resolution="1280x720")
 stream = io.BytesIO()
 time.sleep(1.5)
 
@@ -16,9 +16,8 @@ with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
     connection, address = sock.accept()
     with connection:
         while True:
-            data = connection.recv(1024)
-            if not data:
-                break
             camera.capture(stream, "jpeg")
             print("about to send...")
-            connection.sendall(b"test")
+            connection.sendall(stream.getvalue())
+            time.sleep(0.2)
+
